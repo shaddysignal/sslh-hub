725426ee5fbc2f05c91b14f9597262dfd1ede464
lawtancool <26829131+lawtancool@users.noreply.github.com>
2020-02-04 21:51:43 -0800

    add CMD_OPTS environment variable to entrypoint

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sslh -f -u root --listen $LISTEN_IP:$LISTEN_PORT \
+sslh $CMD_OPTS -f -u root --listen $LISTEN_IP:$LISTEN_PORT \
    --ssh $SSH_HOST:$SSH_PORT \
    --ssl $HTTPS_HOST:$HTTPS_PORT \
    --openvpn $OPENVPN_HOST:$OPENVPN_PORT \
